83a00ce941b1af265b0cab63e9e3c3f2d9dd2384
brachintosh <bedendo.br@gmail.com>
2022-06-21 15:22:58 -0300

    setting-browser-router

--- desafio-react-bedendo/src/App.js
+++ desafio-react-bedendo/src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>Hola brachintosh!</h1>
         <img src="https://img.icons8.com/office/16/000000/starred-ticket.png" className="logo-ticket" alt="logo" />
-        
+      <header className="App-header">
       </header>
+        
     </div>
   );
 }
--- desafio-react-bedendo/src/index.js
+++ desafio-react-bedendo/src/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 import './index.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// React version 18
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      {/* <Route path="details" element={<Details />} /> */}
+      {/* <Route path="favourites" element={<Favourite />} /> */}
+      
+      {/* NOT FOUND  */}
+      <Route path="*" 
+        element={
+        <main style={{ padding: "1rem" }}>
+          <p>There's nothing here!</p>
+          <Link  style={{ 
+            border:'solid #343434 1px', borderRadius:'8px', textDecoration:'none', fontSize: '2em',
+            color: '#343434', display: "block", margin: "1rem 0", backgroundColor:'tomato',
+            paddingLeft: '1.4rem', width:'12vw' 
+            }} to={`/`}
+          >
+            Go Home
+          </Link>
+        </main>
+        }
+      />
+
+    </Routes>
+  </BrowserRouter>
 );
