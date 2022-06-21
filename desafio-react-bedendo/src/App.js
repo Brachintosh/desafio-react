4eeb6f8a5b99384feaf401d0991c7400bc62b6e4
brachintosh <bedendo.br@gmail.com>
2022-06-21 12:23:43 -0300

    react-boiler-plate

---
+++ desafio-react-bedendo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hola brachintosh!</h1>
+        <img src="https://img.icons8.com/office/16/000000/starred-ticket.png" className="logo-ticket" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
